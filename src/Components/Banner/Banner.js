910abd8590d8ba03a209e0372fa934de0411ee06
nahasmampad <nahasbinasker@gmail.com>
2022-10-25 17:33:04 +0530

    first commit

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import './Banner.css'
+import axios from '../../axios'
+import {API_KEY, imageUrl} from '../../constants/constants'
+
+function Banner() {
+
+  const [movie,setMovie] = useState('')
+
+useEffect(()=>{
+
+    axios.get (`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      
+      setMovie(response.data.results[0])
+      console.log(movie, '123');
+
+    })
+
+  },[])
+  return (
+    <div className='banner' style={{backgroundImage:`url( ${movie ? imageUrl + movie.backdrop_path : ''})`}}>
+        <div className='content'>
+        <h1 className='title'>{movie ? movie.title : ""} </h1>
+            <div className='banner-buttons'>
+                <button>Play</button>
+                <button>My list</button>
+
+            </div>
+            <h1 className='discription'>{movie ? movie.overview : ""}</h1>
+        </div>
+
+        <div className="fade"></div>
+
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3'
+export const API_KEY = 'a914e46a505482fa6f306ba72809e448'
+export const imageUrl= 'https://image.tmdb.org/t/p/original'
